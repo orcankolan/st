b28a88fc92a86bd39465ceb6bba49a1103de1cc1
Orcan Kolankaya <orcankolankaya@gmail.com>
2019-10-04 23:12:09 +0300

    remove st Alt+p keybinding

--- config.h
+++ config.h
@@ -234,7 +234,6 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Insert,      clippaste,      {.i =  0} },
 	{ MODKEY,               XK_c,           clipcopy,       {.i =  0} },
 	{ MODKEY,               XK_v,           clippaste,      {.i =  0} },
-	{ MODKEY,               XK_p,           selpaste,       {.i =  0} },
 	{ MODKEY,               XK_Num_Lock,    numlock,        {.i =  0} },
 	{ MODKEY,               XK_Control_L,   iso14755,       {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
